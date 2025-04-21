75896c9e24266104fcbe2e344c4a16e3dda8916d
Mohit_tripathi <mohit.tripathi@teqfocus.com>
2025-04-21 16:20:28 +0530

    Adding template file to improve the flow towards swagger

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from app.routes import coins
 from dotenv import load_dotenv
 import os
+from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
 
 load_dotenv()
 
@@ -21,6 +23,12 @@ def version_info():
         "app_version": "1.0.0",
         "coingecko_api": os.getenv("COINGECKO_API_BASE", "not set"),
     }
+
+
+@app.get("/", response_class=HTMLResponse)
+def read_root():
+    with open("templates/index.html", "r", encoding="utf-8") as f:
+        return f.read()
 
 
 app.include_router(coins.router, prefix="/api")
